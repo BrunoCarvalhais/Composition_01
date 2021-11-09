06c87202677a4d499e1ded0b9443edba10c81b1e
BrunoCarvalhais <carvalhaisazevedo@gmail.com>
2021-11-09 13:14:53 -0300

    Third Commit

--- src/entities/Worker.java
+++ src/entities/Worker.java
@@ -3,6 +3,7 @@ package entities;
 import entities.enums.WorkerLevel;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class Worker {
@@ -70,5 +71,20 @@ public class Worker {
     //Removendo o contrato da lista de contratos
     public void removeContract(HourContract contract) {
         contracts.remove(contract);
+    }
+
+    //Método de cálculo de quanto o trabalhador ganhou em um ano e no mês
+    public double income(int year, int month){
+        double sum = basesalary;
+        Calendar cal = Calendar.getInstance();
+        for (HourContract c : contracts){
+            cal.setTime(c.getDate());
+            int c_year = cal.get(Calendar.YEAR);
+            int c_month = 1 + cal.get(Calendar.MONTH);
+            if (year == c_year && month == c_month){
+                sum += c.totalValue();
+            }
+        }
+        return sum;
     }
 }
